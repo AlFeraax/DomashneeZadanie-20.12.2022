e9e7417cb5916d8625a637679012f4bf02abd9c3
AlFeraax <grigolin@mail.ru>
2022-12-20 23:42:21 +0300

    25 задача сделана + исправления

--- Zadanie25/Program.cs
+++ Zadanie25/Program.cs
@@ -4,12 +4,12 @@ Console.WriteLine("Введите второе число:");
 int chislo2 = Convert.ToInt32(Console.ReadLine());
 int Stepen(int chislo1, int chislo2)
 {
-int res = 1;
-for (int i = 1; i <= chislo2; i++)
-{
-res = res * chislo1;
-}
-return res;
+    int res = 1;
+    for (int i = 1; i <= chislo2; i++)
+    {
+        res = res * chislo1;
+    }
+    return res;
 }
 int stepen = Stepen(chislo1, chislo2);
 Console.WriteLine("Итогом возведения числа " + chislo1 + " в степень числа " + chislo2 + " является число " + stepen);
